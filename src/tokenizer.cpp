b3eee504db81f6759b552baee150ae2ee8a3dd83
Jonathan WEBSTER <64645223+JWR26@users.noreply.github.com>
2024-11-29 00:00:35 +0100

    corrected increment order for temporary string iterator

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -115,10 +115,11 @@ namespace cpplox {
                     if (next(temp) == '\0') {
                         errors::unterminated_string(line);
                     } else {
-                        std::shared_ptr<Token> t = std::make_shared<Token>(TOKEN_TYPE::STRING, it, std::distance(it, temp) + 2, start_line);
+                        ++temp;
+                        std::shared_ptr<Token> t = std::make_shared<Token>(TOKEN_TYPE::STRING, it, std::distance(it, temp) + 1, start_line);
                         t->string = std::string(it+1, temp);
                         tokens.push_back(t);
-                        ++temp;
+
                     }
                     it = temp;
                     break;
