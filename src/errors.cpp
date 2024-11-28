8a25c9d717f02f2cf01c0825698143e1f7829700
Jonathan WEBSTER <64645223+JWR26@users.noreply.github.com>
2024-11-28 22:02:15 +0100

    exit code attempt

--- src/errors.cpp
+++ src/errors.cpp
@@ -5,7 +5,7 @@ namespace cpplox {
     namespace errors {
         
         void unexpected_character(const int& line, const char& c) {
-            std::cout << "[line " << line << "] Error: Unexpected character: " << c << "\n";
+            std::cerr << "[line " << line << "] Error: Unexpected character: " << c << "\n";
         }
 
     }
