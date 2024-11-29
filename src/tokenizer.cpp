0e85278028ea912dff3a119c84c578d288f45884
Jonathan WEBSTER <64645223+JWR26@users.noreply.github.com>
2024-11-29 22:39:11 +0100

    identifiers logic changed

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -160,7 +160,7 @@ namespace cpplox {
                 default:
                     if (isalpha(*it) || *it == '_') {
                         std::string::const_iterator start = it;
-                        while (isalpha(*it) || *it == '_' || !isspace(next(it)) || next(it) != '\0') {
+                        while (isalpha(next(it)) || next(it) == '_') {
                             ++it;
                         }
                         tokens.emplace_back(std::make_shared<Token>(TOKEN_TYPE::IDENTIFIER, start, std::distance(start, it) + 1, line));
