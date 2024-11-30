b0f23700cac9b7d3b768ca946072d6dc841100a7
Jonathan WEBSTER <64645223+JWR26@users.noreply.github.com>
2024-11-30 01:24:54 +0100

    groupings

--- src/parser.cpp
+++ src/parser.cpp
@@ -43,7 +43,7 @@ namespace cpplox{
         if((*it)->tokentype == TOKEN_TYPE::LEFT_PAREN){
             Expr_ptr expr = expression(++it);
             ++it;
-            return std::make_shared<grouping>(expr);
+            return std::make_shared<grouping>(*expr);
         }
         return std::make_shared<literal>();
     }
